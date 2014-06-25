89b3088398afd4d4f583e24ba99c8a00a06e67f6
Itamar Syn-Hershko <itamar@code972.com>
2014-06-25 20:36:22 +0300

    Support multiple types and indices in a Search request

--- NElasticsearch/Commands/SearchCommand.cs
+++ NElasticsearch/Commands/SearchCommand.cs
@@ -11,36 +11,58 @@ namespace NElasticsearch.Commands
         public static SearchResponse<T> Search<T>(this ElasticsearchRestClient client,
             object query, string indexName = null, string typeName = null) where T : new()
         {
-            var response = client.Execute<SearchResponse<T>>(GetSearchRequest(query, indexName, typeName));
-            if (response.StatusCode != HttpStatusCode.OK)
-                throw ElasticsearchException.CreateFromResponseBody(response.Content);
-            if (response.ErrorException != null)
-                throw new Exception("NElasticsearch internal error", response.ErrorException);
+            return Search<T>(client, query, (indexName == null) ? null : new[] { indexName }, (typeName == null) ? null : new[] { typeName });
+        }
+
+        public static SearchResponse<T> Search<T>(this ElasticsearchRestClient client,
+            object query, string[] indexNames = null, string[] typeNames = null) where T : new()
+        {
+            var response = client.Execute<SearchResponse<T>>(GetSearchRequest(query, indexNames, typeNames));
+            VerifySearchResponse(response);
             return response.Data;
         }
 
         public static string Search(this ElasticsearchRestClient client,
             object query, string indexName = null, string typeName = null)
         {
-            var response = client.Execute(GetSearchRequest(query, indexName, typeName));
+            return Search(client, query, (indexName == null) ? null : new[] {indexName}, (typeName == null) ? null : new[] {typeName});
+        }
+
+        public static string Search(this ElasticsearchRestClient client,
+            object query, string[] indexNames = null, string[] typeNames = null)
+        {
+            var response = client.Execute(GetSearchRequest(query, indexNames, typeNames));
+            VerifySearchResponse(response);
+            return response.Content;
+        }
+
+        internal static void VerifySearchResponse(IRestResponse response)
+        {
             if (response.StatusCode != HttpStatusCode.OK)
                 throw ElasticsearchException.CreateFromResponseBody(response.Content);
             if (response.ErrorException != null)
                 throw new Exception("NElasticsearch internal error", response.ErrorException);
-            return response.Content;
         }
 
-        private static RestRequest GetSearchRequest(object query, string indexName = null, string typeName = null)
+        private static RestRequest GetSearchRequest(object query, string[] indexNames = null, string[] typeNames = null)
         {
             var sb = new StringBuilder();
-            if (!string.IsNullOrWhiteSpace(indexName))
+            if (indexNames != null)
             {
-                sb.Append(indexName);
+                for (var i = 0; i < indexNames.Length; i++)
+                {
+                    if (i > 0) sb.Append(',');
+                    sb.Append(indexNames[i]);
+                }
                 sb.Append('/');
             }
-            if (!string.IsNullOrWhiteSpace(typeName))
+            if (typeNames != null)
             {
-                sb.Append(typeName);
+                for (var i = 0; i < typeNames.Length; i++)
+                {
+                    if (i > 0) sb.Append(',');
+                    sb.Append(typeNames[i]);
+                }
                 sb.Append('/');
             }
             sb.Append("_search");
