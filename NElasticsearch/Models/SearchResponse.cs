7708bcdb8c0295ee5f35fb26bcad3f5548c6f372
Itamar Syn-Hershko <itamar@code972.com>
2014-06-26 03:36:02 +0300

    Initial and naive support for aggregations

--- NElasticsearch/Models/SearchResponse.cs
+++ NElasticsearch/Models/SearchResponse.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.Net;
 
 namespace NElasticsearch.Models
@@ -15,5 +16,18 @@ namespace NElasticsearch.Models
         public Hits<T> hits { get; set; }
 
         public HttpStatusCode status { get; set; } // TODO always null
+
+        public Dictionary<string, AggregationResultHolder> aggregations { get; set; }
+    }
+
+    public class AggregationResultHolder
+    {
+        public class Bucket
+        {
+            public string Key { get; set; }
+            public int DocCount { get; set; }
+        }
+
+        public List<Bucket> Buckets { get; set; }
     }
 }
