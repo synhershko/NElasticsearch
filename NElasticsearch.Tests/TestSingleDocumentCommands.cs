79965466b66c71d38089ed239d4f7398d7556b29
Itamar Syn-Hershko <itamar@code972.com>
2014-08-10 19:30:24 +0300

    Ignore test which can't run on MyGet

--- NElasticsearch.Tests/TestSingleDocumentCommands.cs
+++ NElasticsearch.Tests/TestSingleDocumentCommands.cs
@@ -6,7 +6,7 @@ namespace NElasticsearch.Tests
 {
     public class TestSingleDocumentCommands
     {
-        [Fact]
+        [Fact(Skip = "Ignored for build servers with ES running locally")]
         public void Can_add_get_and_delete()
         {
             var client = new ElasticsearchRestClient("http://localhost:9200");
