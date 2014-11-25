28d5fdb4b24a1ced547250b720985cd4ba25e064
Itamar Syn-Hershko <itamar@code972.com>
2014-11-25 23:03:32 +0200

    Cleanup + pass parameters to inner RestClients

--- NElasticsearch/ElasticsearchRestClient.cs
+++ NElasticsearch/ElasticsearchRestClient.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
-using System.Threading;
-using System.Threading.Tasks;
 using RestSharp;
 using RestSharp.Deserializers;
 
@@ -24,7 +22,16 @@ namespace NElasticsearch
         {
             foreach (var elasticsearchUrl in elasticsearchUrls)
             {
-                var internalClient = new RestClient(elasticsearchUrl.ToString());
+                var internalClient = new RestClient(elasticsearchUrl.ToString())
+                {
+                    Authenticator = Authenticator,
+                    CookieContainer = CookieContainer,
+                    Proxy = Proxy,
+                    UserAgent = UserAgent,
+                    UseSynchronizationContext = UseSynchronizationContext,
+                    Timeout = Timeout,
+                    ClientCertificates = ClientCertificates,
+                };
                 internalClient.ClearHandlers();
                 internalClient.AddHandler("application/json", new JsonDeserializer());
                 internalClient.AddHandler("text/json", new JsonDeserializer());
@@ -72,120 +79,13 @@ namespace NElasticsearch
                 }
                 return rsp;
             }
-        }
-
-        public Uri BuildUri(IRestRequest request)
-        {
-            throw new NotImplementedException();
-        }
-
-        public RestRequestAsyncHandle ExecuteAsyncGet(IRestRequest request, Action<IRestResponse, RestRequestAsyncHandle> callback, string httpMethod)
-        {
-            throw new NotImplementedException();
-        }
-
-        public RestRequestAsyncHandle ExecuteAsyncPost(IRestRequest request, Action<IRestResponse, RestRequestAsyncHandle> callback, string httpMethod)
-        {
-            throw new NotImplementedException();
-        }
-
-        public RestRequestAsyncHandle ExecuteAsyncGet<T>(IRestRequest request, Action<IRestResponse<T>, RestRequestAsyncHandle> callback, string httpMethod)
-        {
-            throw new NotImplementedException();
-        }
-
-        public RestRequestAsyncHandle ExecuteAsyncPost<T>(IRestRequest request, Action<IRestResponse<T>, RestRequestAsyncHandle> callback, string httpMethod)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IRestResponse ExecuteAsGet(IRestRequest request, string httpMethod)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IRestResponse ExecuteAsPost(IRestRequest request, string httpMethod)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IRestResponse<T> ExecuteAsGet<T>(IRestRequest request, string httpMethod) where T : new()
-        {
-            throw new NotImplementedException();
-        }
-
-        public IRestResponse<T> ExecuteAsPost<T>(IRestRequest request, string httpMethod) where T : new()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IRestResponse<T>> ExecuteTaskAsync<T>(IRestRequest request, CancellationToken token)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IRestResponse<T>> ExecuteTaskAsync<T>(IRestRequest request)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IRestResponse<T>> ExecuteGetTaskAsync<T>(IRestRequest request)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IRestResponse<T>> ExecuteGetTaskAsync<T>(IRestRequest request, CancellationToken token)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IRestResponse<T>> ExecutePostTaskAsync<T>(IRestRequest request)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IRestResponse<T>> ExecutePostTaskAsync<T>(IRestRequest request, CancellationToken token)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IRestResponse> ExecuteTaskAsync(IRestRequest request, CancellationToken token)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IRestResponse> ExecuteTaskAsync(IRestRequest request)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IRestResponse> ExecuteGetTaskAsync(IRestRequest request)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IRestResponse> ExecuteGetTaskAsync(IRestRequest request, CancellationToken token)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IRestResponse> ExecutePostTaskAsync(IRestRequest request)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IRestResponse> ExecutePostTaskAsync(IRestRequest request, CancellationToken token)
-        {
-            throw new NotImplementedException();
-        }
+        }        
 
         public CookieContainer CookieContainer { get; set; }
         public string UserAgent { get; set; }
         public int Timeout { get; set; }
         public bool UseSynchronizationContext { get; set; }
         public IAuthenticator Authenticator { get; set; }
-        public string BaseUrl { get; set; }
-        public IList<Parameter> DefaultParameters { get; private set; }
         public X509CertificateCollection ClientCertificates { get; set; }
         public IWebProxy Proxy { get; set; }
 
